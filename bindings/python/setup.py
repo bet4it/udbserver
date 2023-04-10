0d7d932a69552689b3c23476f25d7ebdbf94c365
Bet4 <0xbet4@gmail.com>
2023-04-10 22:15:56 +0800

    Build with setuptools-rust

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,90 +1,25 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-
-from setuptools import setup, Extension
-from pathlib import Path
 import os
-import shutil
-import sys
-import subprocess
-from distutils.command.build import build
-from setuptools.command.sdist import sdist
-from setuptools.command.bdist_egg import bdist_egg
+import sysconfig
+from setuptools import setup, find_packages
+from setuptools_rust import Binding, RustExtension
+from setuptools.command.build_ext import build_ext
+from wheel.bdist_wheel import bdist_wheel
 
-ROOT_DIR = Path(os.path.realpath(__file__)).parent
-BUILD_DIR = Path(ROOT_DIR) / "python_build" / "usr"
+class CustomBuildExt(build_ext):
+    def get_ext_filename(self, ext_name):
+        filename = super().get_ext_filename(ext_name)
+        suffix = sysconfig.get_config_var('EXT_SUFFIX')
+        ext = os.path.splitext(filename)[1]
+        return filename.replace(suffix, '') + ext
 
-VERSION = "0.1"
+class CustomBdistWheel(bdist_wheel):
+    def get_tag(self):
+        _, _, plat = super().get_tag()
+        return ('py3', 'none', plat)
 
-def cargo_clean():
-    args = ["cargo", "clean"]
-    subprocess.check_output(args, cwd=ROOT_DIR)
-
-def cargo_cinstall():
-    args = ["cargo", "cinstall"]
-    if not os.getenv("DEBUG", ""):
-        args += ["--release"]
-    args += ["--prefix", "/usr", "--destdir", "python_build"]
-    subprocess.check_output(args, cwd=ROOT_DIR)
-
-
-class custom_sdist(sdist):
-    def run(self):
-        return sdist.run(self)
-
-class custom_build(build):
-    def run(self):
-        cargo_clean()
-        cargo_cinstall()
-        return build.run(self)
-
-class custom_bdist_egg(bdist_egg):
-    def run(self):
-        self.run_command('build')
-        return bdist_egg.run(self)
-
-cmdclass = {}
-cmdclass['build'] = custom_build
-cmdclass['sdist'] = custom_sdist
-cmdclass['bdist_egg'] = custom_bdist_egg
-
-extra_link_args = {
-    "linux" : ["-Wl,--no-as-needed", "-l:libunicorn.so"],
-    "linux2" : ["-Wl,--no-as-needed", "-l:libunicorn.so"],
-    "darwin": ["-lunicorn"]
-}
-
-
-rust_module = Extension('udbserver_rust',
-                        include_dirs=[str(BUILD_DIR / "include")],
-                        sources=['udbserver.c'],
-                        library_dirs=["/usr/local/lib"], # Assume unicorn is install globally as cargo.toml suggests
-                        extra_link_args=extra_link_args.get(sys.platform, extra_link_args['linux']),
-                        extra_objects=[str(BUILD_DIR / "lib" / "libudbserver.a")],
-                        )
-
-setup (name = 'udbserver',
-       version = VERSION,
-       author = 'Bet4',
-       author_email = '0xbet4@gmail.com',
-       description = 'Python bindings of udbserver',
-       url = 'https://github.com/bet4it/udbserver',
-       license='MIT License',
-       classifiers=[
-           'Intended Audience :: Developers',
-           'License :: OSI Approved :: MIT License',
-           'Programming Language :: Python :: 3',
-           'Topic :: Software Development :: Debuggers',
-       ],
-       ext_modules = [rust_module],
-       packages=["udbserver"],
-       cmdclass=cmdclass,
-       install_requires=[
-        "unicorn>=2.0.0rc7"
-       ],
-       py_modules = [],
-       is_pure=False,
-       package_data={
-           "udbserver" : ["udbserver.c"]
-       }
+setup(
+    name='udbserver',
+    packages=find_packages(),
+    rust_extensions=[RustExtension('udbserver.libudbserver', binding=Binding.NoBinding, path='../../Cargo.toml', features=['capi'])],
+    cmdclass={'build_ext': CustomBuildExt, 'bdist_wheel': CustomBdistWheel},
 )
--- bindings/python/udbserver/__init__.py
+++ bindings/python/udbserver/__init__.py
@@ -1,7 +1,15 @@
-from unicorn import *
-from udbserver_rust import udbserver as udb
+import os
+import ctypes
+from unicorn import Uc
+from ctypes import c_void_p, c_uint16, c_uint64
+
+_current_dir = os.path.dirname(os.path.abspath(__file__))
+_udbserver_lib = ctypes.cdll.LoadLibrary(os.path.join(_current_dir, "libudbserver.so"))
+
+_udbserver_lib.udbserver.argtypes = [c_void_p, c_uint16, c_uint64]
+_udbserver_lib.udbserver.restype = None
 
 def udbserver(uc: Uc, port: int = 1234, start_addr: int = 0):
     """Start udbserver.
     """
-    return udb(int(uc._uch.value), port, start_addr)
+    _udbserver_lib.udbserver(int(uc._uch.value), port, start_addr)
